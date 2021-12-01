ea6ef07591b5bc5800f01f49c0632395f1d2ec4f
kozmazoltanbalazs <kozma.z.balazs@gmail.com>
2021-12-01 22:51:58 +0100

    day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,48 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun part1(input: List<Int>): Int {
+        var timesIncreased = 0
+        var previousNum: Int? = null
+        for (value in input) {
+            when {
+                previousNum == null -> println("$value (N/A - no previous measurement)")
+                previousNum < value -> {
+                    println("$value (increased)")
+                    timesIncreased++
+                }
+                previousNum > value -> println("$value (decreased)")
+                previousNum == value -> println("$value (no change)")
+            }
+            previousNum = value
+        }
+        return timesIncreased
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        var timesIncreased = 0
+        var previousSum: Int? = null
+        for ((index, value) in input.withIndex()) {
+            val value1 = value
+            val value2 = input.getOrNull(index + 1) ?: continue
+            val value3 = input.getOrNull(index + 2) ?: continue
+            val sumOfValues = value1 + value2 + value3
+            when {
+                previousSum == null -> println("$sumOfValues (N/A - no previous sum)")
+                previousSum < sumOfValues -> {
+                    println("$sumOfValues (increased)")
+                    timesIncreased++
+                }
+                previousSum > sumOfValues -> println("$sumOfValues (decreased)")
+                previousSum == sumOfValues -> println("$sumOfValues (no change)")
+            }
+            previousSum = sumOfValues
+        }
+        return timesIncreased
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
+        .mapNotNull { value -> value.toIntOrNull() }
+
     println(part1(input))
     println(part2(input))
 }
